239f8955aea776be23422aab7842f6278c8a4a18
Brblo22 <bruno.blomqvist@edu.orebro.se>
2026-05-21 13:39:33 +0200

    Add files via upload

---
+++ maträttModel.cs
@@ -0,0 +1,8 @@
+﻿namespace skolmaten.Models
+{
+    public class maträttModel
+    {
+        public string Dag { get; set; } = "";
+        public List<string> Maträtter { get; set; } = new List<string>();
+    }
+}
